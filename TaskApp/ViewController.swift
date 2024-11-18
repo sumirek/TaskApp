9850995eb014d3bf30d174b844753a893d9da20e
sumire.kuroda <sumire.a.kuroda@fujifilm.com>
2024-11-18 10:55:49 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by UI3 on 2024/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
